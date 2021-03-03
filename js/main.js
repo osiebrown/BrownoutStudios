f91a713b0beaa0efee370ba0fb1ed0833ee36704
Libia Perez <alexandra220790@gmail.com>
2021-03-03 20:49:28 -0300

    add js fo play video en iphone and ipad

--- js/main.js
+++ js/main.js
@@ -1,6 +1,1 @@
-$(document).ready(function () {
-  $(".navbar-xbootstrap").click(function () {
-    $(".nav-xbootstrap").toggleClass("visible");
-    $("body").toggleClass("cover-bg");
-  });
-});
+var vid = document.getElementById("vid").play();
